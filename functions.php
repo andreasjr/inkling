a29d569cf005e191299a7f3a4b660adfccfdac4a
Andreas Reif <andreas@nysics.com>
2023-05-15 11:16:42 -0600

    Remove test code

--- functions.php
+++ functions.php
@@ -9,11 +9,3 @@ function concept_enqueue_scripts() {
 	wp_enqueue_style( 'concept-styles', get_template_directory_uri() . '/style.css', array(), filemtime(get_theme_file_path('/style.css')) );
 }
 endif;
-
-
-function filter_theme_json_theme( $theme_json ){
-	// echo('<pre>');
-	// print_r($theme_json);
-	// echo('</pre>');
-}
-// add_filter( 'wp_theme_json_data_theme', 'filter_theme_json_theme' );
