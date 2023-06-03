b19c7c42e342c13c8d744b6cd21335ebc0092769
Andreas Reif <andreas@nysics.com>
2023-06-03 15:07:30 -0600

    Now enqueues minified styles

--- functions.php
+++ functions.php
@@ -6,6 +6,6 @@ add_action( 'enqueue_block_editor_assets',  'inkling_enqueue_scripts' );
 // Enqueue scripts and styles
 if (!function_exists('inkling_enqueue_scripts')) :
 function inkling_enqueue_scripts() {
-	wp_enqueue_style( 'inkling-styles', get_template_directory_uri() . '/style.css', array(), filemtime(get_theme_file_path('/style.css')) );
+	wp_enqueue_style( 'inkling-styles', get_template_directory_uri() . '/style.min.css', array(), filemtime(get_theme_file_path('/style.min.css')) );
 }
 endif;
